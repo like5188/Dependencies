d10f40dd16cd014e2d02b26182bf162c41eb0685
like5188 <260864866@qq.com>
2021-07-07 17:27:12 +0800

    完善Testing

--- dependencies/src/main/java/com/like/dependencies/Testing.kt
+++ dependencies/src/main/java/com/like/dependencies/Testing.kt
@@ -2,11 +2,11 @@ package com.like.dependencies
 
 object Testing {
     object Java {
-        const val jUnit = "junit:junit:4.13.2"
+        const val junit = "junit:junit:4.13.2"
     }
 
-    object Android {
-        const val androidx_junit = "androidx.test.ext:junit:1.1.3"
+    object AndroidX {
+        const val junit = "androidx.test.ext:junit:1.1.3"
         const val espresso_core = "androidx.test.espresso:espresso-core:3.4.0"
     }
 }
